bbeeddca8dedfc6e7d557d5dccb9ac68bedb43ce
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-25 20:59:48 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,26 @@
 class Waiter
-
+  
+  @@all = []
+  attr_accessor :name, :years
+  def initialize(name, years)
+    @name = name
+    @years = years
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_meal
+    
+  end
+  
+  def meals
+    
+  end
+  
+  def best_tipper
+    
+  end
 end
