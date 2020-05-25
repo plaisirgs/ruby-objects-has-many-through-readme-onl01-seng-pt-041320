00448bb2e42f925cc362b42ed2008d601331df39
GINELLE PLAISIR <plaisirginelle@gmail.com>
2020-05-25 21:03:00 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,8 +17,10 @@ class Waiter
   end
   
   def meals
-    
+    Meal.all.select do |meal|
+    meal.waiter == self #checking for waiter now
   end
+end
   
   def best_tipper
    best_tipped_meal = meals.max do |meal_a, meal_b|
